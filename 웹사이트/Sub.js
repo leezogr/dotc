2161caa3b3dd0b4e1aec4fb740d67a776212a515
leezogr <59490554+leezogr@users.noreply.github.com>
2020-11-14 17:53:32 +0900

    Add files via upload

---
+++ 웹사이트/Sub.js
@@ -0,0 +1,18 @@
+$(document ).ready(function(){
+
+
+
+        $.ajax({     
+        url:'http://apis.data.go.kr/1360000/VilageFcstInfoService/getUltraSrtNcst?serviceKey=06HtAy90bTzoxi7t4CKHr0lNBSmEJFaFoOvTKauqerULvclubEnOfHLE4cPvbnLidAtoF69Uj4lJPKRsozpNMg%3D%3D&numOfRows=10&pageNo=1&base_date=20201113&base_time=0600&nx=55&ny=127',
+        dataType: 'XML',
+        type: 'GET',
+        success: function(data){
+            
+            
+
+
+        }
+    })
+                var $temp = 'sososo'
+                        console.log($temp)
+});
